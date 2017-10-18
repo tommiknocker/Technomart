96201aa43dcb204bd15d267d9a7a7d65039c3346
Elena Shagaeva <30930203+tommiknocker@users.noreply.github.com>
2017-10-18 23:13:41 +0300

    Technomart
    Разметка двух страниц.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+'use strict';
+/*
+var guarantItem = document.querySelector(".services-item-guarantee");
+var guarant = document.querySelector(".services-guarantee");
+guarantItem.addEventListener("click", function (evt) {
+evt.preventDefault();
+guarant.classList.toggle("guarantee-show");})
