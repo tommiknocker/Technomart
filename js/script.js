0860d5d92c232c7a9e5b8abb767c2b358a3ab313
Elena Shagaeva <30930203+tommiknocker@users.noreply.github.com>
2017-11-09 11:52:24 +0300

    index
    всплывающие окна товаров и ховеры

--- js/script.js
+++ js/script.js
@@ -4,4 +4,23 @@ var guarantItem = document.querySelector(".services-item-guarantee");
 var guarant = document.querySelector(".item-guarantee");
 guarantItem.addEventListener("click", function (evt) {
 evt.preventDefault();
-guarant.classList.toggle("guarantee-show");})
+guarant.classList.toggle("item-show");})
+
+var shipItem = document.querySelector(".services-item-shipping");
+var ship = document.querySelector(".item-shipping");
+shipItem.addEventListener("click", function (evt) {
+evt.preventDefault();
+ship.classList.toggle("item-show");})
+
+var writeUs = document.querySelector(".contacts-btn");
+var openForm = document.querySelector(".modal-form-unit");
+var login = openForm.querySelector("[name=username]");
+
+writeUs.addEventListener("click", function (evt) {
+evt.preventDefault();
+openForm.classList.toggle("form-show");
+login.focus();});
+
+window.addEventListener("keydown", function (evt) {
+if (evt.keyCode === 27) {
+openForm.classList.remove("form-show");}   });
